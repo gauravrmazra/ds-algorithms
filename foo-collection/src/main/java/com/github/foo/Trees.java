cb38fd222b4d1124a20f64710df34831885fb6ba
Sachin Pandey <sachinp054@gmail.com>
2017-05-09 11:35:28 +0530

    Trees class consisting static methods to operate of Tree

---
+++ foo-collection/src/main/java/com/github/foo/Trees.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.github.foo;
+
+/**
+ * Class consists static methods that operates on 
+ * Tree. 
+ * <p>The methods of this class all throw a <tt>NullPointerException</tt>
+ * if the collections or class objects provided to them are null.</p>
+ * 
+ * @author Sachin
+ * @author Gaurav Rai Mazra
+ */
+public class Trees {
+
+	
+}
